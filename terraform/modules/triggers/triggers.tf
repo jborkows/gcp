3eec092ec402bceedc6573749e10ca7ec5da9c64
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-03-10 19:10:34 +0100

    rest of triggers

--- terraform/modules/triggers/triggers.tf
+++ terraform/modules/triggers/triggers.tf
@@ -20,3 +20,51 @@ resource "google_cloudbuild_trigger" "terraform" {
         }
     }
 }
+
+
+resource "google_cloudbuild_trigger" "firebase" {
+  name = "firebase"
+  description = "firebase website"
+  project = var.project_id
+  provider = google-beta
+  filename = "firebase/cloudbuild.json"
+  service_account = var.service_account
+  ignored_files   = ["firebase/Dockerfile", "firebase/myapp/package.json", "firebase/cloudbuild-react.json"]
+    included_files  = [
+        "firebase/**",
+    ]
+   github {
+        name  = var.repository_name
+        owner = var.owner
+
+        push {
+            branch       = "^main$"
+            invert_regex = false
+        }
+    }
+}
+
+
+resource "google_cloudbuild_trigger" "firebase-react-build" {
+  name = "firebase-react-build"
+  description = "base image for react code"
+  project = var.project_id
+  provider = google-beta
+  filename = "firebase/cloudbuild-react.json"
+  service_account = var.service_account
+  ignored_files   = []
+    included_files  = [
+        "firebase/Dockerfile",
+         "firebase/myapp/package.json", 
+         "firebase/cloudbuild-react.json"
+    ]
+   github {
+        name  = var.repository_name
+        owner = var.owner
+
+        push {
+            branch       = "^main$"
+            invert_regex = false
+        }
+    }
+}
