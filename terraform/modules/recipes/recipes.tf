073fd7f4087a36aa501ebacc9c4003ec2f5c62bd
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-03-20 00:03:30 +0100

    recipes - gcloud run try

---
+++ terraform/modules/recipes/recipes.tf
@@ -0,0 +1,85 @@
+resource "google_service_account" "recipes_worker" {
+  account_id   = "recipes-worker"
+  display_name = "Recipes worker SA"
+}
+
+# Set permissions
+resource "google_project_iam_binding" "service_permissions" {
+  for_each = toset([
+    "run.invoker"
+  ])
+
+  role       = "roles/${each.key}"
+  members    = [local.recipes_worker_sa]
+  depends_on = [google_service_account.recipes_worker]
+}
+
+
+# The Cloud Run service
+resource "google_cloud_run_service" "recipes" {
+  name                       = "recipes"
+  location                   = var.region
+  autogenerate_revision_name = true
+
+  template {
+    spec {
+      service_account_name = google_service_account.recipes_worker.email
+      containers {
+        image = data.external.image_digest.result.image
+        # env {
+        #   name  = "BUCKET_NAME"
+        #   value = google_storage_bucket.media.name
+        # }
+        # env {
+        #   name  = "FUNCTION_NAME"
+        #   value = google_cloudfunctions_function.function.https_trigger_url
+        # }
+      }
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+
+ metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale"      = "5"
+        "run.googleapis.com/client-name"        = "terraform"
+      }
+    }
+
+  depends_on = [google_project_service.run]
+}
+
+# Set service public
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.recipes.location
+  project  = google_cloud_run_service.recipes.project
+  service  = google_cloud_run_service.recipes.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+  depends_on  = [google_cloud_run_service.recipes]
+}
+
+
+# WORKAROUND 
+data "external" "image_digest" {
+  program = ["bash", "../scripts/get_latest_tag.sh", var.project, "recipes"]
+}
+# END WORKAROUND
+
+locals {
+  deployment_name = "recipes"
+  recipes_worker_sa  = "serviceAccount:${google_service_account.recipes_worker.email}"
+}
---
+++ terraform/modules/recipes/variables.tf
@@ -0,0 +1,16 @@
+variable "repository_name" {
+    type = string
+}
+
+variable "owner" {
+    type = string
+}
+
+variable "project_id" {
+    type = string
+}
+
+
+variable "service_account" {
+    type = string
+}
