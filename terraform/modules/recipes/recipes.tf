c753f7a685d1b245e9a50809e96c7c9eeb83d8e3
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-03-20 00:17:27 +0100

    recipes - gcloud run try

--- terraform/modules/recipes/recipes.tf
+++ terraform/modules/recipes/recipes.tf
@@ -75,7 +75,7 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
 
 # WORKAROUND 
 data "external" "image_digest" {
-  program = ["bash", "../scripts/get_latest_tag.sh", var.project_id, "recipes"]
+  program = ["bash", "scripts/get_latest_tag.sh", var.project_id, "recipes"]
 }
 # END WORKAROUND
 
