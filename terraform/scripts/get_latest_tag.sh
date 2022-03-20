1f52d017d317b92909d0092c51f521e33cf88677
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-03-20 01:41:32 +0100

    recipes - gcloud run try

--- terraform/scripts/get_latest_tag.sh
+++ terraform/scripts/get_latest_tag.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # WORKAROUND SCRIPT
 
@@ -17,6 +17,6 @@ PROJECT=$1
 IMAGE=$2
 
 # deep JSON is invalid for terraform, so serve flat value
-LATEST=$(gcloud container images describe gcr.io/${PROJECT}/${IMAGE}:latest  --format="value(image_summary.fully_qualified_digest)" | tr -d '\n')
+LATEST=`gcloud container images describe gcr.io/${PROJECT}/${IMAGE}:latest  --format="value(image_summary.fully_qualified_digest)" | tr -d '\n'`
 # echo "{\"image\": \"${LATEST}\"}"
 echo "{\"project\": \"${PROJECT}\", \"image\": \"${LATEST}\", \"name\": \"${IMAGE}\"}"
