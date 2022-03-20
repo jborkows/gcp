1f0793ed4d46446572679355edce00c4e9696f12
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-03-20 01:56:07 +0100

    recipes - gcloud run try

--- terraform/modules/recipes/recipes.tf
+++ terraform/modules/recipes/recipes.tf
@@ -55,8 +55,8 @@ data "google_iam_policy" "noauth" {
   }
 }
 
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  region = google_cloud_run_service.recipes.location
+resource "google_cloud_run_service_iam_policy" "recipes_noauth" {
+#   region = google_cloud_run_service.recipes.location
   project  = google_cloud_run_service.recipes.project
   service  = google_cloud_run_service.recipes.name
 
