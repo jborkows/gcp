e5584e928030846426483be13678bedc8db46adc
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-03-20 01:26:46 +0100

    recipes - gcloud run try

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,7 +45,7 @@ data "google_service_account" "gcp_account" {
 }
 # WORKAROUND 
 data "external" "recipes_digest" {
-  program = ["bash", "${path.module}/scripts/get_latest_tag.sh", var.project_id, "recipes"]
+  program = ["/bin/bash", "scripts/get_latest_tag.sh", var.project_id, "recipes"]
 }
 # END WORKAROUND
 
