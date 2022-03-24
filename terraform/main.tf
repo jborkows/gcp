f8727b618ca4bc79d1fe7252882861927f61a826
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-03-24 23:59:29 +0100

    trying

--- terraform/main.tf
+++ terraform/main.tf
@@ -108,7 +108,7 @@ module "recipes"{
    region=var.region
   #  image=data.external.recipes_digest.result.image
   #  image="gcr.io/coastal-idea-336409/recipes@sha256:9cde27f716e5ea54eca1903f2747167dd439c91f4f3be1740c463637873d3e55"
-   image="gcr.io/coastal-idea-336409/recipes@latest"
+   image="gcr.io/coastal-idea-336409/recipes:latest"
 }
 
 
