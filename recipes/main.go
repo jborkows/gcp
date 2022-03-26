24444333627f030e429b5699f71eae101dd7814e
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-03-26 12:18:23 +0100

    triggering build...

--- recipes/main.go
+++ recipes/main.go
@@ -30,5 +30,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
         if name == "" {
                 name = "World"
         }
-        fmt.Fprintf(w, "AAAA22a %s!\n", name)
+        fmt.Fprintf(w, "AAA %s!\n", name)
 }
