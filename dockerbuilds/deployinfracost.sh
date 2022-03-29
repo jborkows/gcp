fa79fb2878293449def9f61a639cb325f68c86d1
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-03-29 23:06:12 +0200

    snyk

--- dockerbuilds/deployinfracost.sh
+++ dockerbuilds/deployinfracost.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export version=0.2
+export version=0.4
 docker build -t gcr.io/$PROJECT_ID/infracostbuild:${version} -f infracost.dockerfile . --build-arg PROJECT_ID=$PROJECT_ID --build-arg TERRAFORM_BUILDER_VERSION=0.1
 docker push gcr.io/$PROJECT_ID/infracostbuild:${version}
