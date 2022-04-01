a0cee1b3c4dacea0b14d45254161a748e7f6c1b9
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-04-02 01:14:16 +0200

    moving into artifact repository part 1a - recipes test

--- recipes/main.go
+++ recipes/main.go
@@ -123,5 +123,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("User ID: %v\n", user)
 
-	fmt.Fprintf(w, "AAasA %s %s %s %s !\n", name, "verified", user.PhoneNumber, user.Email)
+	fmt.Fprintf(w, "XXXX %s %s %s %s !\n", name, "verified", user.PhoneNumber, user.Email)
 }
