0a75caadc27c8fd5b93d743ccbd815a6fd8ad2f3
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-04-02 00:28:15 +0200

    moving into artifact repository part 1a - test

--- terraform/modules/triggers/triggers.tf
+++ terraform/modules/triggers/triggers.tf
@@ -255,12 +255,12 @@ resource "google_cloudbuild_trigger" "recipes" {
     }
     step {
       name    = "gcr.io/cloud-builders/docker"
-      args    = ["build", "-t", "$$_MYREPO/recipes:latest", "."]
+      args    = ["build", "-t", "$${_MYREPO}/recipes:latest", "."]
       dir     = "recipes"
     }
     step {
       name    = "gcr.io/cloud-builders/docker"
-      args    = ["push", "$$_MYREPO/recipes:latest", "."]
+      args    = ["push", "$${_MYREPO}/recipes:latest", "."]
       dir     = "recipes"
     }
 
