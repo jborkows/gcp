77bbeaa59cf14289e588a611978dbfe3a982601a
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-04-20 08:04:05 +0200

    frontend lambda

--- firebase/src/header/UserShortInfo.tsx
+++ firebase/src/header/UserShortInfo.tsx
@@ -4,15 +4,12 @@ import { useRouter } from 'next/router'
 
 export const UserShortInfo = () => {
     const router = useRouter()
-    const {authenticated, data} = useAuthentication()
+    const { authenticated, data } = useAuthentication()
 
-    const redirectToLogin = ()=>{
-        router.push('/login')
-    }
-
+    const redirectToLogin = () => router.push('/login')
 
     return (
-        <div className={styles.UserShortInfo} onClick={()=>redirectToLogin()}>
+        <div className={styles.UserShortInfo} onClick={() => redirectToLogin()}>
             {authenticated && <p className={styles.username}>{data.username}</p>}
             {authenticated && data.photoUrl && <img className={styles.image} src={data.photoUrl}></img>}
             {!authenticated && <p className={styles.login}>Log in</p>}
