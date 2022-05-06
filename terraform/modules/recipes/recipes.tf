d1ba1515bb2448be158051e508774d73ddb07437
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-05-07 00:19:07 +0200

    Concurrency level of service

--- terraform/modules/recipes/recipes.tf
+++ terraform/modules/recipes/recipes.tf
@@ -29,6 +29,7 @@ resource "google_cloud_run_service" "recipe_svc" {
   template {
     spec {
       service_account_name = google_service_account.recipes_worker.email
+      container_concurrency = 3
       containers {
         image = "${var.repository_info.image_prefix}/${var.recipes_image_name}:${data.external.recipes_image_tag.result.tag}"
         env {
