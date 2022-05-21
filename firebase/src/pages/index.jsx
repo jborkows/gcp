6b6cc92fe2fd139291e7925477d7eb07f91632ad
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2022-05-21 23:33:54 +0200

    reload page issue

--- firebase/src/pages/index.jsx
+++ firebase/src/pages/index.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import MyApp from '../components/MyApp'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 
 export default function Home() {
+  const router = useRouter()
+  useEffect(() => {
+    if (router.asPath === '/') {
+      return;
+    }
+    router.push(router.asPath)
+
+  }, [router])
   return (
     <div>Strona główna</div>
   )
